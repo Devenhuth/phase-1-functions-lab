b2fc7847b4c860c6d9a58cff9d702a1549fb325a
Deven <devenzachuth@gmail.com>
2022-04-08 10:37:43 -0500

    commit tree b08aacbdb771a1d20d4236da6083c2b210a4c87e

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution in this file!
+const headquarters = 42
+
+function distanceFromHqInBlocks(someValue) {
+        return Math.abs(someValue - headquarters)
+}
+
+function distanceFromHqInFeet(someValue) {
+        return distanceFromHqInBlocks(someValue) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+    return (destination - start) *264
+    }
+    else{
+        return (start - destination) *264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (start < destination) {
+        let distance = (destination - start) *264
+            if (distance <= 400) {
+                return 0
+            }
+            if (distance > 2500) {
+                return "cannot travel that far"
+            }
+            if (distance > 2000) {
+                return 25
+            }
+            else {
+                return (distance -400) * 0.02
+            }
+        }
+    else{
+        let distance = (start - destination) *264
+        if (distance <= 400) {
+            return 0
+         }
+         if (distance > 2500) {
+             return "cannot travel that far"
+         }
+         if (distance > 2000) {
+            return 25
+        }
+         else {
+            return (distance - 400) * 0.02
+        }
+}
+}
